c847a7b38a98551b0bcd43177926b8a995d7cd70
TylerHJH <15083628369@163.com>
2017-05-24 14:12:36 +0800

    Test

--- project/Start.java
+++ project/Start.java
@@ -2,7 +2,7 @@ package project;
 
 public class Start {
 
-	public static void main(String[] args) 
+//	public static void main(String[] args) 
 	{
 		// TODO Auto-generated method stub
 		Login start = new Login();
