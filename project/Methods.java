5ebfcea8aefce12d67588263e442f5ffb31efc45
Stevenmsh <1512224016@qq.com>
2017-05-28 19:44:28 +0800

    目前

--- project/Methods.java
+++ project/Methods.java
@@ -321,9 +321,9 @@ public class Methods
 									Date date = new Date(c.get(Calendar.YEAR),c.get(Calendar.MONTH),c.get(Calendar.DATE),
 											c.get(Calendar.HOUR_OF_DAY),c.get(Calendar.MINUTE),c.get(Calendar.SECOND));
 								
-									Order order = new Order(passengerName, identityID, passengerID, order.getSeat() + 1, flight,
+									Order order = new Order(passengerName, identityID, passengerID, flight.getSeatNumber(), flight,
 											date, "Paid" , temp1, temp2, temp3);
-									//(乘客ID，座位号，航班号， 时间，状态 , 乘客名称，身份证号，起飞年月日)
+									//(乘客名称，身份证号，乘客ID，座位号，航班号， 时间，状态 , 起飞年月日)
 									
 									order.setSeat(order.getSeat() + 1);
 									Data.ListOfOrder.add(order);
